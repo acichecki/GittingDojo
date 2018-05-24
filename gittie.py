5c5259905f342d5e57d3a2229265e0647ac33325
adamcichecki <a.cichecki@gmail.com>
2018-05-24 09:57:56 +0200

    edited

--- gittie.py
+++ gittie.py
@@ -1,11 +1,13 @@
 def introduce():
-    print("Hello, I'm Gittie!")
+    print("Hello, I'm Gittie and the bees!")
 
-<<<<<<< HEAD
+
 def add(a,b):
     c = a + b
     return c
-git
-=======
+
 def joke():
->>>>>>> f0e78cbb6198ea4a034b0eb9c387cf274a9a90be
+
+
+
+    print('joke')
